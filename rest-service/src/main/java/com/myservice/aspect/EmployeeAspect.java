b1d8bdec0d200376a5816c3a577710c34b36faa0
ramch <ramchjob@gmail.com>
2015-10-19 18:37:35 +0530

    Added spring aop changes for component scan

--- rest-service/src/main/java/com/myservice/aspect/EmployeeAspect.java
+++ rest-service/src/main/java/com/myservice/aspect/EmployeeAspect.java
@@ -3,7 +3,10 @@ package com.myservice.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
+import org.springframework.stereotype.Component;
+
 @Aspect
+@Component
 public class EmployeeAspect {
 
    @Before("execution(* getEmployee(..)) && args(param) ")
